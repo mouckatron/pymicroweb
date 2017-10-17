f590c1fb6c794392e5490d6c9e40152c8ff1ffee
Graham <mouckatron@gmail.com>
2017-10-17 18:12:08 +0100

    Handle MIME types and return Content-Type header

--- microweb.py
+++ microweb.py
@@ -5,11 +5,83 @@ import logging
 import logging.handlers
 import os.path as op
 import Queue
+import re
 import socket
 import threading
 
+MIME_TYPES = {
+    'aac': 'audio/aac',
+    'abw': 'application/x-abiword',
+    'arc': 'application/octet-stream',
+    'avi': 'video/x-msvideo',
+    'azw': 'application/vnd.amazon.ebook',
+    'bin': 'application/octet-stream',
+    'bz': 'application/x-bzip',
+    'bz2': 'application/x-bzip2',
+    'csh': 'application/x-csh',
+    'css': 'text/css',
+    'csv': 'text/csv',
+    'doc': 'application/msword',
+    'eot': 'application/vnd.ms-fontobject',
+    'epub': 'application/epub+zip',
+    'gif': 'image/gif',
+    'htm': 'text/html',
+    'html': 'text/html',
+    'ico': 'image/x-icon',
+    'ics': 'text/calendar',
+    'jar': 'application/java-archive',
+    'jpeg': 'image/jpeg',
+    'jpg': 'image/jpeg',
+    'js': 'application/javascript',
+    'json': 'application/json',
+    'mid': 'audio/midi',
+    'midi': 'audio/midi',
+    'mpeg': 'video/mpeg',
+    'mpkg': 'application/vnd.apple.installer+xml',
+    'odp': 'application/vnd.oasis.opendocument.presentation',
+    'ods': 'application/vnd.oasis.opendocument.spreadsheet',
+    'odt': 'application/vnd.oasis.opendocument.text',
+    'oga': 'audio/ogg',
+    'ogv': 'video/ogg',
+    'ogx': 'application/ogg',
+    'otf': 'font/otf',
+    'png': 'image/png',
+    'pdf': 'application/pdf',
+    'ppt': 'application/vnd.ms-powerpoint',
+    'rar': 'application/x-rar-compressed',
+    'rtf': 'application/rtf',
+    'sh': 'application/x-sh',
+    'svg': 'image/svg+xml',
+    'swf': 'application/x-shockwave-flash',
+    'tar': 'application/x-tar',
+    'tif': 'image/tiff',
+    'tiff': 'image/tiff',
+    'ts': 'application/typescript',
+    'ttf': 'font/ttf',
+    'vsd': 'application/vnd.visio',
+    'wav': 'audio/x-wav',
+    'weba': 'audio/webm',
+    'webm': 'video/webm',
+    'webp': 'image/webp',
+    'woff': 'font/woff',
+    'woff2': 'font/woff2',
+    'xhtml': 'application/xhtml+xml',
+    'xls': 'application/vnd.ms-excel',
+    'xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'xml': 'application/xml',
+    'xul': 'application/vnd.mozilla.xul+xml',
+    'zip': 'application/zip',
+    '3gp': 'video/3gpp',
+    '3g2': 'video/3gpp2',
+    '7z': 'application/x-7z-compressed'
+}
+
 
 class HTTPRequest(object):
+
+    uri_re = re.compile('^([^?]*)\??([^#]*)#?(.*)$')
+    fileext_re = re.compile('^.*\.([a-zA-Z]{2,5})$')
+
     def __init__(self, request):
         lines = request.splitlines()
         method, request_uri, http_version = lines[0].split()
@@ -20,6 +92,8 @@ class HTTPRequest(object):
         self.http_version = http_version
 
         self.headers = {}
+        self.uri = {'path': 'index.html', 'query': '', 'fragment': '', 'fileext': ''}
+        self.parse_uri()
 
         for line in lines:
             try:
@@ -29,11 +103,25 @@ class HTTPRequest(object):
             else:
                 self.headers[_header] = _value
 
+    def parse_uri(self):
+        if self.request_uri == '':
+            self.uri['fileext'] = HTTPRequest.fileext_re.match(self.uri['path']).group(1)
+            return
+
+        _parse = HTTPRequest.uri_re.match(self.request_uri)
+
+        self.uri['path'] = _parse.group(1)
+        self.uri['query'] = _parse.group(2)
+        self.uri['fragment'] = _parse.group(3)
+        self.uri['fileext'] = HTTPRequest.fileext_re.match(self.uri['path']).group(1)
+
     def __str__(self):
-        return "Method: {} Request URI: {} HTTP Version: {} Headers={}".format(self.method,
-                                                                       self.request_uri,
-                                                                       self.http_version,
-                                                                       self.headers)
+        return "Method: '{}' Request URI: '{}' Parsed URI: '{}' HTTP Version: '{}' Headers={}".format(
+            self.method,
+            self.request_uri,
+            self.uri,
+            self.http_version,
+            self.headers)
 
 
 class HTTPResponse(object):
@@ -44,14 +132,13 @@ class HTTPResponse(object):
         self.body = ""
         self.headers = {'Server': 'pymicroweb'}
 
-
     def setbody(self, data):
         self.body = data
         self.headers['Content-Length'] = len(self.body.encode('utf-8'))
 
     def setheader(self, header, value):
         self.headers[header] = value
-        
+
     def __str__(self):
         return "{} {} {}\n{}\n\n{}".format(self.http_version,
                                            self.response_code,
@@ -94,11 +181,16 @@ class HTTPWorker(threading.Thread):
 
     def get(self, request, response):
         try:
-            response.setbody(open(op.join(self.wwwdir, request.request_uri)).read())
-        except  IOError:
-            self.log.error("File Not Found: %s", op.join(self.wwwdir, request.request_uri))
+            response.setbody(open(op.join(self.wwwdir, request.uri['path'])).read())
+        except IOError:
+            self.log.error("File Not Found: %s", op.join(self.wwwdir, request.uri['path']))
             response.response_code = 404
             response.response_message = "File not found"
+        else:
+            try:
+                response.setheader('Content-Type', MIME_TYPES[request.uri['fileext']])
+            except KeyError:
+                self.log.error("No MIME type for %s", request.uri['fileext'])
 
 
 if __name__ == '__main__':
